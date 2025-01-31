ffc8e24ce4d21855380ac83b52e77157e59a8e14
bc-rep-project <johannderbiel@gmail.com>
2025-01-31 21:15:48 +0200

    Remove service worker registration code

--- src/service-worker.js
+++
@@ -1,6 +0,0 @@
-// Register service worker in your index.js or App.js
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js');
-  });
-} 
